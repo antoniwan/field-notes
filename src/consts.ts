abf25ab17ef508b023f8080c5e2f6398a49f1028
Antonio Rodríguez Martínez <antoniwan@icloud.com>
2025-09-13 13:14:29 -0400

    Update site description and enhance About and Index pages: Revise SITE_DESCRIPTION for clarity, update titles and subtitles in the About and Index pages to better reflect the content's focus on human experience and open-source principles, and improve text formatting for emphasis.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Field Notes on (Modern) Humanity';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'An open-source manual for being human';
